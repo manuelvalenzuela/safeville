c4a8c2c5f96dfc433a7cc263dd78004c46641dd7
Manuel Valenzuela V <manuel@dreamsit.cl>
2019-10-13 15:53:34 -0300

    Invite user to Join a Community Use Case

---
+++ src/SafeVille.Core/UseCases/InviteUserToJoinACommunityUseCase.cs
@@ -0,0 +1,22 @@
+﻿namespace SafeVille.Core.UseCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Dtos.Out;
+    using Exceptions;
+
+    public static class InviteUserToJoinACommunityUseCase
+    {
+        public static async Task<VehicleRegistered> Invite(object o)
+        {
+            if (o == null)
+            {
+                throw new AppArgumentException(nameof(o));
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/SafeVille.Tests/UseCases/InviteUserToJoinACommunityUseCaseTests.cs
@@ -0,0 +1,20 @@
+﻿namespace SafeVille.Tests.UseCases
+{
+    using System;
+    using System.Threading.Tasks;
+    using Core.Exceptions;
+    using Dtos.Out;
+    using FluentAssertions;
+    using SafeVille.Core.UseCases;
+    using Xunit;
+
+    public class InviteUserToJoinACommunityUseCaseTests
+    {
+        [Fact]
+        public void InviteNull_ShouldThrowException()
+        {
+            Func<Task<VehicleRegistered>> action = async () => await InviteUserToJoinACommunityUseCase.Invite(null);
+            action.Should().Throw<AppArgumentException>();
+        }
+    }
+}
